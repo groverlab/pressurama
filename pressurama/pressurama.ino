6f13484ee6d65f9d75eb1c87a060488f8221805c
William H. Grover <wgrover@gmail.com>
2021-06-14 11:15:14 -0700

    Create pressurama.ino

---
+++ pressurama/pressurama.ino
@@ -0,0 +1,23 @@
+// From the datasheet:  https://www.nxp.com/docs/en/data-sheet/MPX4250D.pdf
+// Vout = VCC × (P × 0.00369 + 0.04)
+// P = pressure in kPa
+
+void setup() {
+  Serial.begin(2000000);
+}
+
+int number_of_measurements = 100;
+float sum = 0;
+float Vout = 0;
+const float Vcc = 5.0;
+float P = 0;
+
+void loop() {
+  sum = 0;
+  for (int i = 0; i < number_of_measurements; i++) {
+    sum = sum + analogRead(A0);
+  }
+  Vout = 5.0 * (sum/number_of_measurements) / 1024.0;
+  P = ((Vout/Vcc) - 0.04) / 0.00369;
+  Serial.println(P);
+}
