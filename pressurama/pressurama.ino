e4717aa4cab611e3483c016cde6846c4468220e9
Mabel Shehada <msheh002@ucr.edu>
2021-08-19 16:34:13 -0700

    Update pressurama.ino
    Updated to send serial plotter data to python

--- pressurama/pressurama.ino
+++ pressurama/pressurama.ino
@@ -2,11 +2,13 @@
 // Vout = VCC × (P × 0.00369 + 0.04)
 // P = pressure in kPa
 
-int aiPins[] = {0};
+int aiPins[] = {0,1,2,3,4,5,6,7};
 int pinCount = 8;
+unsigned long timer = 0;
+long loopTime = 5000;   // microseconds
 
 void setup() {
-  Serial.begin(2000000);
+  Serial.begin(38400);
 }
 
 int number_of_measurements = 100;
@@ -27,4 +29,41 @@ void loop() {
     Serial.print("\t");
   }
   Serial.println();
+
+    timeSync(loopTime);
+  //int val = analogRead(0) - 512;
+  double val = (analogRead(0) -512) / 512.0;
+  sendToPC(&val);
+}
+
+void timeSync(unsigned long deltaT)
+{
+  unsigned long currTime = micros();
+  long timeToDelay = deltaT - (currTime - timer);
+  if (timeToDelay > 5000)
+  {
+    delay(timeToDelay / 1000);
+    delayMicroseconds(timeToDelay % 1000);
+  }
+  else if (timeToDelay > 0)
+  {
+    delayMicroseconds(timeToDelay);
+  }
+  else
+  {
+      // timeToDelay is negative so we start immediately
+  }
+  timer = currTime + timeToDelay;
+}
+
+void sendToPC(int* data)
+{
+  byte* byteData = (byte*)(data);
+  Serial.write(byteData, 2);
+}
+
+void sendToPC(double* data)
+{
+  byte* byteData = (byte*)(data);
+  Serial.write(byteData, 4);
 }
