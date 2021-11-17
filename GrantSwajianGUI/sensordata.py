7ad2f07bd83b2d1f8fda7bb6d195c0c3c43ad3a6
Shane Hoang <87776837+shoan012@users.noreply.github.com>
2021-11-16 18:01:20 -0800

    Update sensordata.py

--- GrantSwajianGUI/sensordata.py
+++ GrantSwajianGUI/sensordata.py
@@ -11,18 +11,23 @@ from datetime import date
 today = date.today()
 day = today.strftime("%m%d%Y")
 now = datetime.now()
+
 dt_string = now.strftime("%m/%d/%Y")
 readtime = (now.strftime("%H:%M:%S") )
+dataVec = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"] #new
 
 name = "masterfile" #name the file here
 tracker = 0
-sample_size = 2880    #this number designates data points in the save data (time/1.25)
+sample_size = 1800    #this number designates data points in the save data 
+#If we are getting data each hour use (time/1.125) and edit arduino code delay to be 1125
+#if we are getting data each half hour use (time) and edit arduino code delay to be 1000 
 
 class SensorData(object):
     def __init__(self):
         #make connection to arduino
         try:
             self.ser = serial.Serial('COM4', 9600, timeout = 0.1)
+            # I'm assuming this clears out all of the data beforehand.
             self.ser.flushInput()
             print("Connected")
             print("date and time =", dt_string)
@@ -32,14 +37,15 @@ class SensorData(object):
                 self.ser.flushInput()
                 print("Connected")
             except:
-                print("no Connection")
+                print("No connection")
+                
 
         #total sensors/current sensor
         self.sens_names = ["sensor1","sensor2","sensor3","sensor4","sensor5","sensor6","sensor7","sensor8","time","date"]
         with open(name+".csv", 'a') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(self.sens_names)
-            csvfile.close()
+            #csvfile.close() # may be the reason why masterfile doesn't take in code
         self.current_sens = 0
         self.total_sens = len(self.sens_names) 
 
@@ -69,25 +75,34 @@ class SensorData(object):
                 if(self.current_sens<self.total_sens - 2):
                     self.data[self.current_sens] = np.append(self.data[self.current_sens], db)
                     self.data[self.current_sens] = self.data[self.current_sens][1:self.sample_size]
-                    print(db)    
-                if(self.current_sens==self.total_sens - 1):
-                    self.data[self.current_sens] = np.append(self.data[self.current_sens], dt_string)
-                    self.data[self.current_sens] = self.data[self.current_sens][1:self.sample_size]
-                    print(dt_string)
+                    #print(db)
+                    dataVec[self.current_sens] =  db
+                #print(dataVec)
                 if(self.current_sens==self.total_sens - 2):
                     self.data[self.current_sens] = np.append(self.data[self.current_sens], readtime)
                     self.data[self.current_sens] = self.data[self.current_sens][1:self.sample_size]
-                    print(readtime)  
+                    #print(readtime)  
+                    dataVec[self.current_sens] =  readtime
+                if(self.current_sens==self.total_sens - 1):
+                    self.data[self.current_sens] = np.append(self.data[self.current_sens], dt_string)
+                    self.data[self.current_sens] = self.data[self.current_sens][1:self.sample_size]
+                    #print(dt_string)
+                    dataVec[self.current_sens] =  dt_string
                     tracker+=1
+                    print(dataVec)  
+            
                 if(tracker > sample_size):
                     timeread = (now.strftime("%d_%H_%M_%S") )
                     with open(timeread+".csv", 'w') as csvfile:
                         csvwriter = csv.writer(csvfile)
                         csvwriter.writerow(self.sens_names)
-                        dataF = transpose(self.data)
-                        csvwriter.writerows(dataF)
+                        dataT = transpose(self.data)
+                        csvwriter.writerows(dataT)
+                        with open(name+".csv", 'a') as csvfile:
+                            csvwriter = csv.writer(csvfile)
+                            csvwriter.writerows(dataT)  #Pretransposed information
+                            csvfile.close()
                         csvfile.close()
-                        appendcsv(name,dataF) #Pretransposed information
                         tracker = 0
             self.current_sens+=1
         except Exception as e:
@@ -107,8 +122,6 @@ class SensorData(object):
         with open(filename+day+".csv", 'w') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(self.sens_names)
-            dataF = transpose(self.data)
-            csvwriter.writerows(dataF)
+            dataT = transpose(self.data)
+            csvwriter.writerows(dataT)
             csvfile.close()
-
- 
