80fc0347f0476f64cf38f434e1575ad77bfe2d4d
William H. Grover <wgrover@gmail.com>
2021-12-04 13:32:42 -0800

    Update plotter.py

--- wgrover/plotter.py
+++ wgrover/plotter.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 from datetime import datetime
 infile = open("out.csv", "r")
+num_channels = 8
 times = []
-channels = [[] for _ in range(8)]
+channels = [[] for _ in range(num_channels)]
 for line in infile:
     tokens = line.split(",")
     times.append(datetime.fromisoformat(tokens[0]))
-    for i in range(8):
+    for i in range(num_channels):
         channels[i].append(float(tokens[i+1]))
-for i in range(8):
+for i in range(num_channels):
     plt.plot(times, channels[i], label=i)
 plt.legend()
 plt.show()
