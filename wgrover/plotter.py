49d6abd98607f3d603920e79b1d281a917f8f58d
William H. Grover <wgrover@gmail.com>
2021-12-04 13:30:02 -0800

    plotter redone as list comprehension

--- wgrover/plotter.py
+++ wgrover/plotter.py
@@ -2,21 +2,14 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 infile = open("out.csv", "r")
 times = []
-ch0 = []
-ch1 = []
-ch2 = []
-ch3 = []
-ch4 = []
-ch5 = []
-ch6 = []
-ch7 = []
+channels = [[] for _ in range(8)]
 for line in infile:
     tokens = line.split(",")
     times.append(datetime.fromisoformat(tokens[0]))
-    for i, ch in enumerate((ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7)):
-        ch.append(float(tokens[i+1]))
-for i, ch in enumerate((ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7)):
-    plt.plot(times, ch, label=i)
+    for i in range(8):
+        channels[i].append(float(tokens[i+1]))
+for i in range(8):
+    plt.plot(times, channels[i], label=i)
 plt.legend()
 plt.show()
 
