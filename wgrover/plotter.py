74128124df1da6173e134bd5e03d07b22009e9b9
William H. Grover <wgrover@gmail.com>
2021-12-04 13:26:46 -0800

    added all 8 channels to plotter

--- wgrover/plotter.py
+++ wgrover/plotter.py
@@ -2,11 +2,21 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 infile = open("out.csv", "r")
 times = []
-values = []
+ch0 = []
+ch1 = []
+ch2 = []
+ch3 = []
+ch4 = []
+ch5 = []
+ch6 = []
+ch7 = []
 for line in infile:
     tokens = line.split(",")
     times.append(datetime.fromisoformat(tokens[0]))
-    values.append(float(tokens[1]))
-plt.plot(times, values)
+    for i, ch in enumerate((ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7)):
+        ch.append(float(tokens[i+1]))
+for i, ch in enumerate((ch0, ch1, ch2, ch3, ch4, ch5, ch6, ch7)):
+    plt.plot(times, ch, label=i)
+plt.legend()
 plt.show()
 
