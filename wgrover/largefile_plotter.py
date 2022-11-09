bf0fefeba39ae329bf2fbdabb43166a5ebab75ff
William H. Grover <wgrover@gmail.com>
2022-11-08 21:30:06 -0500

    can now plot a window of times

--- wgrover/largefile_plotter.py
+++ wgrover/largefile_plotter.py
@@ -7,25 +7,25 @@ with open(sys.argv[1], 'r') as fp:
         pass
 print('Total Lines', count + 1)
 lines = count
+begin = 1000
+end = 1100  # was lines
+points = end - begin
 
 infile = open(sys.argv[1], "r")
-num_channels = 5  # was 8
-times = [0] * lines
-channels = [[0]*lines for _ in range(num_channels)]
+num_channels = 8  # was 8
+times = [0] * points
+channels = [[0]*points for _ in range(num_channels)]
 
 for i, line in enumerate(infile):
-    if i < lines:
-        try:
-            tokens = line.split(",")
-            times[i] = datetime.fromisoformat(tokens[0])
-            for j in range(num_channels):
-                channels[j][i] = float(tokens[j+1])
-        except IndexError:
-            print("i = ", i, "  j = ", j)
+    if begin <= i < end:
+        tokens = line.split(",")
+        times[i-begin] = datetime.fromisoformat(tokens[0])
+        for j in range(num_channels):
+            channels[j][i-begin] = float(tokens[j+1])
 
 for i in range(num_channels):
     print("plotting ", i)
     plt.plot(times, channels[i], label=i)
-# plt.legend()
+plt.legend()
 plt.savefig("out.png")
 
