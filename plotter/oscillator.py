687dfffc22720aa49dc59054b9dc395977b2cfe3
William H. Grover <wgrover@gmail.com>
2022-11-09 14:35:06 -0500

    full test of global timing

--- plotter/oscillator.py
+++ plotter/oscillator.py
@@ -4,7 +4,9 @@ plotter.kept_channels = [7,1,3,6,2]
 
 # total lines in file:  14707395
 
-
-plotter.plot(4000, 6000, "seconds", "02.pdf")
-plotter.plot(0, 10000,"minutes", "01.pdf")
+plotter.plot(10101100, 10101200, "seconds", "05.pdf")
+plotter.plot(10101000, 10102000, "seconds", "04.pdf")
+plotter.plot(10100000, 10200000, "minutes", "03.pdf")
+plotter.plot(10000000, 11000000, "hours", "02.pdf")
+plotter.plot(0, 14707395, "days", "01.pdf")
 
