39358d2440855a97cf39ed353a12429f552ca329
William H. Grover <wgrover@gmail.com>
2022-11-21 21:34:59 -0800

    clarifying channel numbers

--- plotter/pulse_data.py
+++ plotter/pulse_data.py
@@ -6,15 +6,30 @@ plotter.kept_channels = [7,1,3,6,2]
 plotter.timing = "local"
 
 # total lines in file:  33984
+# Pressurama channel:	Plot color:					Output number:
+# 7						Blue						2
+# 1						Orange						5
+# 3						Green   <-- held down		3
+# 6						Red							1
+# 2						Purple						4
 
 lines = 33984
 
+roi = [5000, 7000]
+
 plotter.figsize = (8, 1.5)  # 12, 1.5
-plotter.plot([5000, 7000], units="seconds", outfile="pulse_data.png")
-# plotter.plot(roi[1], box=roi[2], units="hours", outfile="02.png")
-# plotter.plot(roi[2], box=roi[3], units="minutes", outfile="03.png")
-# plotter.plot(roi[3], box=roi[4], units="minutes", outfile="04.png")
-# plotter.plot(roi[4], box=roi[5], units="seconds1", outfile="05.png")
+
+plotter.kept_channels = [7]
+plotter.plot(roi, units="seconds", outfile="pulse_data_ch2.png")
+plotter.kept_channels = [1]
+plotter.plot(roi, units="seconds", outfile="pulse_data_ch5.png")
+plotter.kept_channels = [3]
+plotter.plot(roi, units="seconds", outfile="pulse_data_ch3.png")
+plotter.kept_channels = [6]
+plotter.plot(roi, units="seconds", outfile="pulse_data_ch1.png")
+plotter.kept_channels = [2]
+plotter.plot(roi, units="seconds", outfile="pulse_data_ch4.png")
+
 
 # plotter.figsize = (8, 3)  # 12, 5
 # plotter.plot(roi[5], units="seconds2", outfile="06.png")
