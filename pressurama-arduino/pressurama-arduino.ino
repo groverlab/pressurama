4bc14c2d7f2ed5d50e33d97c0b129ca75922744c
William H. Grover <wgrover@gmail.com>
2024-03-18 14:55:53 -0700

    make averages a long

--- pressurama-arduino/pressurama-arduino.ino
+++ pressurama-arduino/pressurama-arduino.ino
@@ -1,26 +1,26 @@
 const byte numChars = 32;
-char receivedChars[numChars];   // an array to store the received data
+char receivedChars[numChars];  // an array to store the received data
 boolean newData = false;
-int averages = 10;             // new for this version
+long averages = 10;  // new for this version
 boolean programmed = false;
 
 void setup() {
   Serial.begin(115200);
-  while(!programmed) {
-      recvWithEndMarker();
+  while (!programmed) {
+    recvWithEndMarker();
     showNewNumber();
   }
 }
 
 void loop() {
-  float sums[] = {0,0,0,0,0,0,0,0};
-  for(int measurement = 0; measurement < averages; measurement++) {
-    for(int pin = 0; pin < 8; pin++) {
+  float sums[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+  for (int measurement = 0; measurement < averages; measurement++) {
+    for (int pin = 0; pin < 8; pin++) {
       sums[pin] += analogRead(pin);
     }
   }
   Serial.print("X ");
-  for(int pin = 0; pin < 8; pin++) {
+  for (int pin = 0; pin < 8; pin++) {
     Serial.print(pin);
     Serial.print(":");
     Serial.print(sums[pin] / averages);
@@ -32,37 +32,36 @@ void loop() {
 
 
 void recvWithEndMarker() {
-    static byte ndx = 0;
-    char endMarker = '\n';
-    char rc;
-    
-    if (Serial.available() > 0) {
-        rc = Serial.read();
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
 
-        if (rc != endMarker) {
-            receivedChars[ndx] = rc;
-            ndx++;
-            if (ndx >= numChars) {
-                ndx = numChars - 1;
-            }
-        }
-        else {
-            receivedChars[ndx] = '\0'; // terminate the string
-            ndx = 0;
-            newData = true;
-        }
+  if (Serial.available() > 0) {
+    rc = Serial.read();
+
+    if (rc != endMarker) {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) {
+        ndx = numChars - 1;
+      }
+    } else {
+      receivedChars[ndx] = '\0';  // terminate the string
+      ndx = 0;
+      newData = true;
     }
+  }
 }
 
 void showNewNumber() {
-    if (newData == true) {
-        averages = 0;             // new for this version
-        averages = atoi(receivedChars);   // new for this version
-        Serial.print("This just in ... ");
-        Serial.println(receivedChars);
-        Serial.print("Data as Number ... ");    // new for this version
-        Serial.println(averages);     // new for this version
-        newData = false;
-        programmed = true;
-    }
+  if (newData == true) {
+    averages = 0;                    // new for this version
+    averages = atol(receivedChars);  // new for this version
+    Serial.print("This just in ... ");
+    Serial.println(receivedChars);
+    Serial.print("Data as Number ... ");  // new for this version
+    Serial.println(averages);             // new for this version
+    newData = false;
+    programmed = true;
+  }
 }
